77ea2ed5e5214b7cc956490453891c30cb9be447
Joakim Børlum <joakim@roevhat.dk>
2014-04-11 12:11:37 +0200

    Goes directly to courses of interest

--- enhance.js
+++ enhance.js
@@ -7,7 +7,14 @@ var userStr = document.querySelector('.moodleuser > .main').innerHTML.trim();
 console.log('Logged in as: ' + userStr);
 
 if (userStr === 'Guest user') {
-	window.location.href = 'https://login.aau.dk/cas/login';
+	window.location.href = 'https://login.aau.dk/cas/login?service=http%3A%2F%2Fwww.moodle.aau.dk%2Flogin%2Findex.php';
+}
+
+if (window.location.hash) {
+	console.log('All good!');
+} else {
+	var linkToCourses = document.querySelector('[href="#semester_category_14A"]');
+	linkToCourses.onclick();
 }
 
 console.log('================================');
